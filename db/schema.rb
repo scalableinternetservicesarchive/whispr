9d888c3346acefbff0a5a1aac4a6481a9f4d0d16
Bryan Pan <bryanpan342@gmail.com>
2019-10-15 12:37:04 -0700

    fixed schema bug - missing end

--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_10_10_225611) do
     
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
 end
