5295aafc2ccba9269042433b064060e3341aaeb7
Bryan Pan <bryanpan342@gmail.com>
2019-10-15 10:35:04 -0700

    filled in phone_regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     before_save { email.downcase!}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    VALID_PHONE_REGEX = [0-9]
+    # VALID_ADDRESS_REGEX = [a-a0-9]
+
 
     validates :name, presence: true, length:  {maximum: 50}
     validates :email, presence: true, length: {maximum: 200}, format: {with: VALID_EMAIL_REGEX }, 
