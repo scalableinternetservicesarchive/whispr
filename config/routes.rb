ac47af471c452fa9eee426497821cb386d801d15
Bryan Pan <bryanpan342@gmail.com>
2019-11-07 14:49:33 -0800

    reorder routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
     get 'edit', to: 'users/registrations#edit'
-    match 'medical' => 'users/registrations#medical', :via => [:get], :as => 'medical' 
     get 'sign_up', to: 'users/registrations#new'
+    match 'medical' => 'users/registrations#medical', :via => [:get], :as => 'medical' 
     get '/users/sign_out' => 'devise/sessions#destroy'
     get 'forgot_password', to: 'users/passwords#new'
     get 'reset_password', to: 'users/passwords#edit'
