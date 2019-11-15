4e537954424645fa1f3ff74f5e81724dd85decb5
Bryan Pan <bryanpan342@gmail.com>
2019-11-14 20:22:23 -0800

    profile pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
                                    dependent:   :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_one_attached :avatar
 
   # Follows a user.
   def follow(other_user)
