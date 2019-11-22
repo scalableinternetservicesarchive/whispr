3fd1689c54b03bf0ce451a77f1e18af190937900
Moo Jin Kim <moojink@g.ucla.edu>
2019-11-22 01:20:05 -0800

    Remove a buggy migration

--- db/migrate/20191122064200_remove_indices_from_health_records.rb
+++
@@ -1,6 +0,0 @@
-class RemoveIndicesFromHealthRecords < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :health_records, name: "index_health_records_on_user_id_and_created_at"
-    remove_index :health_records, name: "index_health_records_on_user_id"
-  end
-end
